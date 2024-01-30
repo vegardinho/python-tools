bb378577d6c5f1e63ad010cd5001dbb3b6822af4
vegardinho <landsverk.vegard@gmail.com>
2024-01-30 20:22:31 +0100

    Change to *args and add tilde expansion

--- i_o_utilities.py
+++ i_o_utilities.py
@@ -1,7 +1,12 @@
 from pathlib import Path
+import os.path
 
+def create_files(*args):
+    for file in args:
+        file_path = Path(os.path.expanduser(file)) #expand tilde and load path
+        file_path.parents[0].mkdir(parents=True, exist_ok=True)
+        file_path.touch(exist_ok=True)
 
-def create_files(files):
-    for file in files:
-        Path(file).parents[0].mkdir(parents=True, exist_ok=True)
-        Path(file).touch(exist_ok=True)
+# TESTING
+if __name__ == '__main__':
+    create_files('~/Downloads/arne/går/mot/enfil.txt')
