8d0b10a7292fa2d98de92b2b40b3fd28ba7d20f2
vegardinho <landsverk.vegard@gmail.com>
2024-02-05 23:36:31 +0100

    Fix bug causing only attachment to be sent, if present

--- send_email.py
+++ send_email.py
@@ -7,21 +7,23 @@ from email.mime.application import MIMEApplication
 
 
 
-def send_email(sender_email, receiver_email, keychain_name, subject, text, files=None):
+def send_email(sender_email, receiver_email, keychain_name, subject, text, *files):
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
     password = keyring.get_password(keychain_name, sender_email)
-    message = MIMEMultipart("alternative")
+    message = MIMEMultipart()
     message["Subject"] = subject
     message["From"] = sender_email
     message["To"] = receiver_email
 
     # Turn these into plain/html MIMEText objects
-    plain_text = MIMEText(text, "plain")
+    # plain_text = MIMEText(text, "plain")
     # Add HTML/plain-text parts to MIMEMultipart message
     # The email client will try to render the last part first
-    message.attach(plain_text)
+    # message.attach(plain_text)
+    message.attach(MIMEText(text))
 
+    # TODO: fix relative paths and tilde expansion
     for f in files or []:
         with open(f, "rb") as fil:
             part = MIMEApplication(
@@ -31,8 +33,7 @@ def send_email(sender_email, receiver_email, keychain_name, subject, text, files
         # After the file is closed
         part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
         message.attach(part)
-
-
+    
     # Create secure connection with server and send email
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
