449311d1b6354fc8cfb00ec11c0c8712b70270fd
vegardinho <landsverk.vegard@gmail.com>
2025-03-03 22:59:53 +0100

    Hide pushover key

--- notify.py
+++ notify.py
@@ -1,19 +1,35 @@
-import http.client, urllib
+import os
+import json
+import http.client
+import urllib
 import send_email
 
 
 def push_notification(text, pushover_token, pushover_key):
-    if pushover_key == None:
-        pushover_key = "uvieox1v1tdsubdnmipw4igx1u65xj"
+    secrets_file = ".secrets"
+    if os.path.exists(secrets_file):
+        with open(secrets_file, "r") as f:
+            secrets = json.load(f)
+            pushover_key = secrets.get("pushover_key", "")
+    else:
+        raise FileNotFoundError(
+            "The .secrets file does not exist and pushover_key is not provided."
+        )
 
     conn = http.client.HTTPSConnection("api.pushover.net:443")
-    conn.request("POST", "/1/messages.json",
-     urllib.parse.urlencode({
-         "token": pushover_token,
-         "user": pushover_key,
-         "message": text,
-         "html": 1, #Enable html formatting
-     }), {"Content-type": "application/x-www-form-urlencoded"})
+    conn.request(
+        "POST",
+        "/1/messages.json",
+        urllib.parse.urlencode(
+            {
+                "token": pushover_token,
+                "user": pushover_key,
+                "message": text,
+                "html": 1,  # Enable html formatting
+            }
+        ),
+        {"Content-type": "application/x-www-form-urlencoded"},
+    )
     conn.getresponse()
 
 
@@ -21,6 +37,6 @@ def mail(recipient, subj, text, files=None):
     send_email.send_email(recipient, subj, text, files)
 
 
-if __name__ == '__main__':
-    text = "Test med html:\n<a href=\"http://example.com/\">word</a>"
+if __name__ == "__main__":
+    text = 'Test med html:\n<a href="http://example.com/">word</a>'
     push_notification(text)
